be05ad2816a5b8fd46c948f4b2aac5b7eaabd076
Bryan Acosta <bxacosta@hotmail.com>
2025-02-06 23:22:55 -0500

    chore: base project

---
+++ src/main/java/com/bxacosta/wpcloudapi/WpCloudApiApplication.java
@@ -0,0 +1,13 @@
+package com.bxacosta.wpcloudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpCloudApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WpCloudApiApplication.class, args);
+    }
+
+}
