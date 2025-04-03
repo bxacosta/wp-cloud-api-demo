cd3605f347205b6088686e45b8bf473dd2643e79
Bryan Acosta <bxacosta@hotmail.com>
2025-04-02 22:19:00 -0500

    feat: add method to register phone number to use the  Cloud API

--- src/main/java/com/bxacosta/wpcloudapi/services/WhatsAppBusinessService.java
+++ src/main/java/com/bxacosta/wpcloudapi/services/WhatsAppBusinessService.java
@@ -1,10 +1,12 @@
 package com.bxacosta.wpcloudapi.services;
 
 import com.bxacosta.wpcloudapi.auth.FacebookAuthService;
+import com.restfb.Body;
 import com.restfb.Connection;
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
 import com.restfb.Version;
+import com.restfb.json.JsonObject;
 import com.restfb.scope.FacebookPermissions;
 import com.restfb.types.DebugTokenInfo;
 import com.restfb.types.GranularScope;
@@ -26,7 +28,8 @@ public class WhatsAppBusinessService {
     /**
      * Retrieves the WhatsApp Business Account (WABA) IDs shared with the user, using the access token obtained
      * after completing the signup flow see
-     * <a href="https://developers.facebook.com/docs/whatsapp/embedded-signup/manage-accounts#get-shared-waba-id-with-access-token">Get Shared WABA ID with Access Token</a>.
+     * <a href="https://developers.facebook.com/docs/whatsapp/embedded-signup/manage-accounts#get-shared-waba-id-with-access-token">
+     * Get Shared WABA ID with Access Token</a>.
      *
      * @param accessToken the access token obtained after signup
      * @return a list of shared WABA IDs, or an empty list if none are found
@@ -90,5 +93,27 @@ public class WhatsAppBusinessService {
         List<WhatsAppMessageTemplate> templates = new ArrayList<>();
         templateConnection.forEach(templates::addAll);
         return templates;
+    }
+
+    /**
+     * Register a phone number to use the WhatsApp Business Cloud API, see
+     * <a href="https://developers.facebook.com/docs/graph-api/reference/whats-app-business-account-to-number-current-status/register/"
+     * WhatsApp Business Phone Number Register</a>.
+     *
+     * @param accessToken   The access token used to authenticate the API request.
+     * @param phoneNumberId The ID of the phone number to be registered.
+     * @throws IllegalStateException If the API returns an unsuccessful result.
+     */
+    public void registerPhoneNumber(String accessToken, String phoneNumberId) {
+        FacebookClient client = new DefaultFacebookClient(accessToken, Version.LATEST);
+
+        JsonObject request = new JsonObject();
+        request.add("pin", "000000");
+        request.add("messaging_product", "whatsapp");
+
+        JsonObject response = client.publish(phoneNumberId + "/register", JsonObject.class, Body.withData(request));
+
+        if (!response.getBoolean("success", false))
+            throw new IllegalStateException("Failed to register phone number with id: " + phoneNumberId);
     }
 }
