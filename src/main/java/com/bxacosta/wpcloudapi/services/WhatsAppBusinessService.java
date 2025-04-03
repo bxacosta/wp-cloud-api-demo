f3acdc20bc5f180f7025d87967a6c894051c8024
Bryan Acosta <bxacosta@hotmail.com>
2025-04-02 21:13:22 -0500

    docs: add javadocs for WhatsAppBusinessService methods

--- src/main/java/com/bxacosta/wpcloudapi/services/WhatsAppBusinessService.java
+++ src/main/java/com/bxacosta/wpcloudapi/services/WhatsAppBusinessService.java
@@ -23,6 +23,14 @@ public class WhatsAppBusinessService {
 
     private final FacebookAuthService facebookAuthService;
 
+    /**
+     * Retrieves the WhatsApp Business Account (WABA) IDs shared with the user, using the access token obtained
+     * after completing the signup flow see
+     * <a href="https://developers.facebook.com/docs/whatsapp/embedded-signup/manage-accounts#get-shared-waba-id-with-access-token">Get Shared WABA ID with Access Token</a>.
+     *
+     * @param accessToken the access token obtained after signup
+     * @return a list of shared WABA IDs, or an empty list if none are found
+     */
     public List<String> getSharedWABAIds(String accessToken) {
         DebugTokenInfo tokenInfo = facebookAuthService.debugToken(accessToken, accessToken);
 
@@ -40,6 +48,15 @@ public class WhatsAppBusinessService {
         return facebookClient.fetchObject(wabaId, WhatsAppBusinessAccount.class);
     }
 
+    /**
+     * Retrieves a list of WhatsApp Business phone numbers associated with a given WABA ID, see
+     * <a href="https://developers.facebook.com/docs/graph-api/reference/whats-app-business-account/phone_numbers/">
+     * WhatsApp Business Account Phone Numbers</a>.
+     *
+     * @param accessToken The access token used to authenticate the Facebook API.
+     * @param wabaId      The ID of the WhatsApp Business Account (WABA) for which the phone numbers are retrieved.
+     * @return A list of {@link WhatsAppBusinessPhoneNumber}.
+     */
     public List<WhatsAppBusinessPhoneNumber> getPhoneNumbersByWABAId(String accessToken, String wabaId) {
         FacebookClient facebookClient = new DefaultFacebookClient(accessToken, Version.LATEST);
 
@@ -53,6 +70,15 @@ public class WhatsAppBusinessService {
         return phoneNumbers;
     }
 
+    /**
+     * Retrieves a list of WhatsApp message templates associated with a specific WABA ID, see
+     * <a href="https://developers.facebook.com/docs/graph-api/reference/whats-app-business-account/message_templates/">
+     * WhatsApp Business Account Message Templates</a>.
+     *
+     * @param accessToken The access token used to authenticate the Facebook API.
+     * @param wabaId      The ID of the WhatsApp Business Account (WABA) for which the message templates are retrieved.
+     * @return A list of {@link WhatsAppMessageTemplate}.
+     */
     public List<WhatsAppMessageTemplate> getMessageTemplatesByWABAId(String accessToken, String wabaId) {
         FacebookClient facebookClient = new DefaultFacebookClient(accessToken, Version.LATEST);
 
